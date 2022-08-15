c2ae40e984237e6ad03e6d3963a0efa7d28abffd
Kostya Petukhov <kosyak971@gmailcom>
2022-08-15 15:26:49 +0300

    1, 2, 3 tasks done

---
+++ script.js
@@ -0,0 +1,99 @@
+﻿'use strict';
+
+// 1. Make a function that displays the number of times it has been called
+
+let counter = 0;
+
+function makeCounter() {
+	counter++;
+	console.log('Function called ' + counter);
+}
+
+makeCounter();
+makeCounter();
+makeCounter();
+
+// 2. Write a function that replaces all ones with zeros and vice versa for the
+// object
+
+const example = {
+	left: {
+		left: {
+			left: 1,
+			right: {
+				left: 0,
+				right: 1,
+			},
+		},
+		right: {
+			left: 0,
+			right: 1,
+		},
+	},
+	right: 1,
+};
+console.log('before', example);
+
+const changeObject = (obj) => {
+	for (const prop in obj) {
+		const value = obj[prop];
+		if (typeof value === 'object') {
+			changeObject(value);
+		} else {
+			obj[prop] = Number(!Boolean(value));
+		}
+	}
+	return obj;
+};
+
+const cloneObj = JSON.parse(JSON.stringify(example));
+changeObject(cloneObj);
+console.log('after', cloneObj);
+
+// 3. Make a function that takes a number n, and returns a two-dimensional array with
+//length n*n, with 2 on the diagonal and 1 on the top and bottom of the resulting
+//segment
+
+const newArr = (size) => {
+	const arr = [];
+	for (let i = 0; i < size; i++) {
+		arr[i] = [];
+		for (let j = 0; j < size; j++) {
+			arr[i][j] =
+				i === j || i === size - j - 1
+					? 2
+					: 0 || (i < j && i < size - j - 1)
+					? 1
+					: 0 || (i > j && i > size - j - 1)
+					? 1
+					: 0;
+		}
+	}
+
+	return arr;
+};
+console.log(newArr(5));
+
+// 4. Create a similar element, when you click on any of the blocks it should change
+// own color
+
+const table = document.getElementById('testTable');
+
+let element;
+let elementId;
+
+table.onmouseover = function getCurrentElement(e) {
+	element = e.target;
+	elementId = e.target.id;
+};
+document.addEventListener('click', function changeColor(e) {
+	if (e.target === element) {
+		e.target.style.backgroundColor = 'aqua';
+		console.log(element);
+	}
+});
+
+table.onmouseout = function removeCurrentElement(e) {
+	const el = document.getElementById(elementId);
+	console.log(el);
+};
